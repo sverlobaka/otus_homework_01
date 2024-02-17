b47b0b8886f35f10c22236171eb3a75697cb6d9c
sverlobaka <sverlobaka@gmail.com>
2024-02-17 20:40:16 +0300

    HW_07

---
+++ homework_07/library/views.py
@@ -0,0 +1,35 @@
+from django.http import HttpRequest
+from django.shortcuts import render
+from .models import Genre, Book
+
+
+def index_view(request):
+    books = (
+        Book
+        .objects.select_related("genre")
+        .all()
+    )
+    context = {
+        "books": books,
+    }
+    return render(
+        request,
+        "library/books-list.html",
+        context
+    )
+
+
+def genres_with_books(request: HttpRequest):
+    context = {
+        "genres": (
+            Genre
+            .objects
+            .prefetch_related("books")
+            .all()
+        )
+    }
+    return render(
+        request,
+        "library/genres-with-books.html",
+        context,
+    )
